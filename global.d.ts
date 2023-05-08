bdaae87e1fc767b558edf12a08d5134a39f2714a
Juan Cortes <j.cortes@crowdfarming.com>
2023-05-08 22:34:29 +0200

    feat: declare interface of ProcessEnv and Request

---
+++ global.d.ts
@@ -0,0 +1,25 @@
+import { UserRole } from '@user/model';
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      DATABASE_URL: string;
+      NEXTAUTH_SECRET: string;
+      NEXTAUTH_URL: string;
+      GOOGLE_CLIENT_ID: string;
+      GOOGLE_CLIENT_SECRET: string;
+    }
+  }
+
+  export interface Request {
+    context: {
+      user?: {
+        email: string;
+        id: string;
+        role: UserRole;
+      };
+    };
+  }
+}
+
+export {};
