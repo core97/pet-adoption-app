0931d8e8168735bbf88b46102025f79d49c511fc
Juan Cortes <j.cortes@crowdfarming.com>
2023-05-23 19:04:29 +0200

    fix: set as multi the breeds select

--- app/pet-ads/creation/page.tsx
+++ app/pet-ads/creation/page.tsx
@@ -1,14 +1,39 @@
-const PetAdCreation = () => (
-  <main
-    style={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '70vh',
-    }}
-  >
-    <h1>Pet ad creation</h1>
-  </main>
-);
+'use client';
+
+import { useState } from 'react';
+import { Container } from '@chakra-ui/react';
+import { FormStep, FormStepper } from '@components';
+import { PetAdForm } from '@pet-ad/presentation/components/PetAdForm';
+import { AddressForm } from '@shared/presentation/components/AddressForm';
+
+const PetAdCreation = () => {
+  const [activeStep, setActiveStep] = useState(0);
+
+  return (
+    <main
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '70vh',
+      }}
+    >
+      <Container maxW="2xl">
+        <FormStepper activeStep={activeStep} onChangeActiveStep={setActiveStep}>
+          <FormStep label="Datos del animal">
+            <PetAdForm
+              onSubmit={console.log}
+              petType="DOG"
+              submitButtonLabel="Submit"
+            />
+          </FormStep>
+          <FormStep label="Dirección">
+            <AddressForm onSubmit={console.log} submitButtonLabel="Submit" />
+          </FormStep>
+        </FormStepper>
+      </Container>
+    </main>
+  );
+};
 
 export default PetAdCreation;
